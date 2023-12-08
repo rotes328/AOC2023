05b90139c98d7d34831079e2bdbfd723b9f3ac17
Michael Rotella <michaelrotella@Michaels-MacBook-Air.local>
2023-12-08 10:45:24 -0500

    Day 8 solution

---
+++ day8/day8.py
@@ -0,0 +1,35 @@
+def get_network():
+    with open('data.txt', 'r', encoding='UTF-8') as data:
+        network = {}
+        path = data.readline().strip()
+        data.readline()
+        for line in data.read().splitlines():
+            key, value = map(str.strip, line.split('='))
+            network[key] = tuple(val.strip('() ') for val in value.split(','))
+    return path, network
+
+
+def format_path(path):
+    path = list(path)
+    return [int(turn.replace('L', '0').replace('R', '1')) for turn in path]
+
+
+def evaulate_network(path, network):
+    steps = 0
+    node = 'AAA'
+    i = 0
+    while node != 'ZZZ':
+        node = network[node][path[i]]
+        steps += 1
+        i = (i + 1) % len(path)
+    return steps
+
+
+def main():
+    path, network = get_network()
+    path = format_path(path)
+    print('Steps in Part 1:', evaulate_network(path, network))
+
+
+if __name__ == '__main__':
+    main()
