a8ed7a58c44097aa12cb1f902b4a4a75ba45c144
Michael Rotella <michaelrotella@Michaels-MacBook-Air.local>
2023-12-09 16:58:06 -0500

    Day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def get_input():
     array = []
     with open('data.txt', 'r', encoding='UTF-8') as data:
@@ -28,15 +30,29 @@ def extrapolate(line, index=None):
     return extrapolate(line, index - 1)
 
 
+def extrapolate_backwards(line, index=None):
+    if index is None:
+        index = len(line) - 1
+
+    if index == 0:
+        return line
+
+    line[index - 1].appendleft(line[index - 1][0] - line[index][0])
+    return extrapolate_backwards(line, index - 1)
+
 
 def main():
     array = get_input()
-    answer = 0
+    answer_p1 = 0
+    answer_p2 = 0
     for line in array:
         analyzed_list = analyze(line)
+        backwards_analyzed_list = [deque(l) for l in analyzed_list]
         analyzed_list[-1].append(0)
-        answer += extrapolate(analyzed_list)[0][-1]
-    print(answer)
+        answer_p1 += extrapolate(analyzed_list)[0][-1]
+        answer_p2 += (extrapolate_backwards(backwards_analyzed_list)[0][0])
+    print('Part 1 Answer:', answer_p1)
+    print('Part 2 Answer:', answer_p2)
 
 
 if __name__ == '__main__':
